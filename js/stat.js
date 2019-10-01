39a575a5053c6c9f755df46e8af25c5e11fd9a32
utavegu <utavegu@list.ru>
2019-10-01 10:18:03 +0500

    Внес правки по заданию 2-1

--- js/stat.js
+++ js/stat.js
@@ -5,37 +5,53 @@ var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var GAP = 10;
-var PLAYER_X = 100;
-var PLAYER_Y = 80;
 var COLUMN_WIDTH = 40;
-var COLUMN_X = 100;
-var COLUMN_Y = 120;
-var INTERVAL = 75; // С 50 не согласен, думаю тут заказчик что-то напутал. По предоставленному макету даже очевидно, что там не 50.
+var COLUMN_HEIGHT = 150;
+var TEXT_X_GAP = 90;
+var TEXT_Y_GAP = 235;
+var FONT_GAP = 5;
 var CONGRATULATION_X = 235;
 var CONGRATULATION_Y = 30;
-var columnHeight = 1;
+var CLOUD_COLOR = '#fff';
+var CLOUD_SHADOW = 'rgba(0, 0, 0, 0.7)';
+var WIN_TEXT = 'Ура вы победили!';
+var RESULT_TEXT = 'Список результатов:';
+var COLOR = '#000';
+var FONT = '16px PT Mono';
 
-function renderCloud(ctx, x, y, color) {
+// ФУНКЦИЯ, рисует само облако статистики
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
-}
+};
 
+// Статистика
+// Рендерит облако
 window.renderStatistics = function (ctx, players, times) {
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, CLOUD_SHADOW);
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_COLOR);
 
-  ctx.fillStyle = '#000';
+  // Текст окна поздравлений
+  ctx.fillStyle = COLOR;
+  ctx.font = FONT;
+  ctx.fillText(WIN_TEXT, CONGRATULATION_X, CONGRATULATION_Y);
+  ctx.fillText(RESULT_TEXT, CONGRATULATION_X - 10, CONGRATULATION_Y + 20);
 
-  ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!', CONGRATULATION_X, CONGRATULATION_Y);
-  ctx.fillText('Список результатов:', CONGRATULATION_X - 10, CONGRATULATION_Y + 20);
+  // Пропорция
+  var maxTime = Math.max.apply({}, times);
+  var proportion = maxTime / COLUMN_HEIGHT;
 
+  // Отрисовка статистики
   for (var i = 0; i < players.length; i++) {
-    ctx.fillText(players[i], PLAYER_X + INTERVAL * (i + 1), PLAYER_Y);
-    ctx.fillText(Math.round(times[i] / 1000) + ' сек.', PLAYER_X + INTERVAL * (i + 1), PLAYER_Y + INTERVAL / 3);
-    if (times[i] >= 15000) {
-      times[i] = 15000;
-    }
-    ctx.fillRect(COLUMN_X + INTERVAL * (i + 1), COLUMN_Y, COLUMN_WIDTH, columnHeight * times[i] / 100);
+    ctx.fillText(players[i], CLOUD_X + GAP * 5 + TEXT_X_GAP * i, GAP * 2 + TEXT_Y_GAP);
+    ctx.fillText(Math.round(times[i]), CLOUD_X + GAP * 5 + TEXT_X_GAP * i, TEXT_Y_GAP - FONT_GAP - (times[i] / proportion));
+    ctx.fillStyle = (players[i] === 'Вы') ? 'rgb(255, 0, 0, 1)' : 'rgb(0, 0,' + Math.random() * 255 + ')';
+    ctx.fillRect(CLOUD_X + GAP * 5 + TEXT_X_GAP * i, TEXT_Y_GAP, COLUMN_WIDTH, -(times[i] / proportion));
+    ctx.font = FONT;
+    ctx.fillStyle = COLOR;
   }
 };
+
+// НАСТАВНИКУ
+// 1) Рекомендации по поводу заворачивания поздравительного текста в функцию понял, но пока не реализую, оставлю это в желательных правках.
+// 2) Есть подозрения, что наплодил лишка констант, в погоне за избавлением от магических чисел. Или норм?
